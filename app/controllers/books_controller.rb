5131f6dd7316713127dd528a450a7f96468e1cd9
gobaru <gobaru.1993@gmail.com>
2021-04-01 23:47:50 +0900

    投稿機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,25 @@
 class BooksController < ApplicationController
+
   def index
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:image, :title, :author, :publisher, :price, :genre_id).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,15 @@
 class Book < ApplicationRecord
 
+  with_options presence: true do
+    validates :title
+    validates :author
+    validates :publisher
+    validates :image
+    validates :price
+    validates :genre_id
+  end
+
   belongs_to :user
-  
+  has_one_attached :image
+
 end
